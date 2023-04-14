0c950bbde7282b9da9eb3369472ae774bf2a5115
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-04-13 23:25:15 -0500

    inicio creacion de menu y header del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        pizarra: "URL('/public/assets/image/background_pizarron.jpg')",
+      },
+    },
   },
   plugins: [],
 };
