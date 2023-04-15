8d4e72ca34def886241094d38b4f48e4f6ef93f4
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-04-14 21:15:15 -0500

    configuracion menu mobile y menu desktop. Aplicación animación menu mobile mediante librería Animate.CSS

--- public/script/main.js
+++ public/script/main.js
@@ -1,1 +1,14 @@
-console.log('Milton Valencia');
+const btnMovile = document.querySelector('#btn_movile');
+const menuMovile = document.querySelector('#menu_movile');
+
+function showMenuMovile() {
+  const isMenuMovileHiden = menuMovile.classList.contains('hidden');
+
+  if (!isMenuMovileHiden) {
+    menuMovile.classList.add('hidden');
+  } else {
+    menuMovile.classList.toggle('hidden');
+  }
+}
+
+btnMovile.addEventListener('click', showMenuMovile);
