7fa07e0e764bc7f6a3cdd1756316dac53b8176d8
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-04-15 21:12:46 -0500

    Configuración script para funcionamiento calculadora

--- public/script/main.js
+++ public/script/main.js
@@ -1,14 +1,101 @@
-const btnMovile = document.querySelector('#btn_movile');
-const menuMovile = document.querySelector('#menu_movile');
+const btnMobile = document.querySelector('#btn_mobile');
+const menuMobile = document.querySelector('#menu_mobile');
+const calculatorScreen = document.querySelector('#calculator_screen');
+const calculatorBtn7 = document.querySelector('#calculatorBtn7');
+const calculatorBtn8 = document.querySelector('#calculatorBtn8');
+const calculatorBtn9 = document.querySelector('#calculatorBtn9');
+const calculatorBtnSuma = document.querySelector('#calculatorBtnSuma');
+const calculatorBtn4 = document.querySelector('#calculatorBtn4');
+const calculatorBtn5 = document.querySelector('#calculatorBtn5');
+const calculatorBtn6 = document.querySelector('#calculatorBtn6');
+const calculatorBtnResta = document.querySelector('#calculatorBtnResta');
+const calculatorBtn1 = document.querySelector('#calculatorBtn1');
+const calculatorBtn2 = document.querySelector('#calculatorBtn2');
+const calculatorBtn3 = document.querySelector('#calculatorBtn3');
+const calculatorBtnMultiplicacion = document.querySelector(
+  '#calculatorBtnMultiplicacion',
+);
+const calculatorBtn0 = document.querySelector('#calculatorBtn0');
+const calculatorBtnReset = document.querySelector('#calculatorBtnReset');
+const calculatorBtnDivision = document.querySelector('#calculatorBtnDivision');
+const calculatorBtnResultado = document.querySelector(
+  '#calculatorBtnResultado',
+);
+let resultado;
 
-function showMenuMovile() {
-  const isMenuMovileHiden = menuMovile.classList.contains('hidden');
+btnMobile.addEventListener('click', () => {
+  const isMenuMobileHidden = menuMobile.classList.contains('hidden');
 
-  if (!isMenuMovileHiden) {
-    menuMovile.classList.add('hidden');
+  // mobile menu config
+  if (!isMenuMobileHidden) {
+    menuMobile.classList.add('hidden');
   } else {
-    menuMovile.classList.toggle('hidden');
+    menuMobile.classList.toggle('hidden');
   }
-}
+});
 
-btnMovile.addEventListener('click', showMenuMovile);
+// calculator config
+calculatorBtn7.addEventListener('click', () => {
+  calculatorScreen.textContent += '7';
+});
+
+calculatorBtn8.addEventListener('click', () => {
+  calculatorScreen.textContent += '8';
+});
+
+calculatorBtn9.addEventListener('click', () => {
+  calculatorScreen.textContent += '9';
+});
+
+calculatorBtnSuma.addEventListener('click', () => {
+  calculatorScreen.textContent += '+';
+});
+
+calculatorBtn4.addEventListener('click', () => {
+  calculatorScreen.textContent += '4';
+});
+
+calculatorBtn5.addEventListener('click', () => {
+  calculatorScreen.textContent += '5';
+});
+
+calculatorBtn6.addEventListener('click', () => {
+  calculatorScreen.textContent += '6';
+});
+
+calculatorBtnResta.addEventListener('click', () => {
+  calculatorScreen.textContent += '-';
+});
+
+calculatorBtn1.addEventListener('click', () => {
+  calculatorScreen.textContent += '1';
+});
+
+calculatorBtn2.addEventListener('click', () => {
+  calculatorScreen.textContent += '2';
+});
+
+calculatorBtn3.addEventListener('click', () => {
+  calculatorScreen.textContent += '3';
+});
+
+calculatorBtnMultiplicacion.addEventListener('click', () => {
+  calculatorScreen.textContent += '*';
+});
+
+calculatorBtn0.addEventListener('click', () => {
+  calculatorScreen.textContent += '0';
+});
+
+calculatorBtnDivision.addEventListener('click', () => {
+  calculatorScreen.textContent += '/';
+});
+
+calculatorBtnReset.addEventListener('click', () => {
+  calculatorScreen.textContent = '';
+});
+
+calculatorBtnResultado.addEventListener('click', () => {
+  resultado = eval(calculatorScreen.textContent);
+  calculatorScreen.textContent = resultado;
+});
